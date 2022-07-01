892216a3ee61e9dac314091c9c738a3f878dbe38
habideen <habideenibrahim@gmail.com>
2022-07-01 12:12:47 -0700

    Print to terminal using 'puts' function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * Main entry point
+ *
+ * Always returns 0
+ *
+ */
 int main(void)
 {
 	char str[] = "Programming is like building a multilingual puzzle";
