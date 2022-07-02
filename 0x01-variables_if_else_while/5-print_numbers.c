a6d1868ee306809bdbfc2156443ebf66700d828d
habideen <habideenibrahim@gmail.com>
2022-07-01 20:43:32 -0700

    Print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 0-9.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+		printf("%n", a++);
+
+	return (0);
+}
