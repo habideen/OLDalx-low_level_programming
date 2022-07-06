7b527a7fa0628ccef6fede26229ac5c9803f7255
habideen <habideenibrahim@gmail.com>
2022-07-05 20:44:15 -0700

    Output with puthar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * Program to print _putchar
+ * File: 0-putchar.c
+ * Author: Habideen Ibrahim
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+
+	return(0);
+}
