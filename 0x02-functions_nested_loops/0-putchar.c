25444e809c859e88e0363c55615c156a071e11e5
habideen <habideenibrahim@gmail.com>
2022-07-05 21:24:29 -0700

    Output with puthar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int a = 0;
+	char str[] = "_putchar";
+
+	while(a < 9)
+	{
+		_putchar(str[a]);
+		a++;
+	}
 
 
 	return(0);
