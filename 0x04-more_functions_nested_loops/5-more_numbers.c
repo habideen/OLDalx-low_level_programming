dd9515ac0e0f2010af7687932ae6490a26d56aa6
habideen <habideenibrahim@gmail.com>
2022-07-07 21:44:44 -0700

    Function that prints number from 0-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,13 @@
 
 void print_most_numbers(void)
 {
-	char x= 0;
+	char x = 0;
 
-	while (x<= 9)
+	while (x <= 9)
 	{
-		if (x!= 2 && x!= 4)
+		if (x != 2 && x != 4)
 		{
-			_putchar('0' +x);
+			_putchar('0' + x);
 		}
 
 		x++;
