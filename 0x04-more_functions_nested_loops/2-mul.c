eeb6f826d76ecde099b74cbbcd95709855c9ee1e
habideen <habideenibrahim@gmail.com>
2022-07-07 21:05:49 -0700

    Function returns the product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - returns the product of 2 arguments passed
+ * @a: int type number
+ * @b: int type number
+ * Return: product
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
