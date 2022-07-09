4a47531a1f4c4ab45bbe54065d49cb98fee85af0
habideen <habideenibrahim@gmail.com>
2022-07-08 21:19:39 -0700

    Print traingle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size : argument
+ * Return:void
+ */
+
+void print_triangle(int size)
+{
+	int i = 1, j;
+
+	while (i <= size && size > 0)
+	{
+		j = 0;
+		while (j < size - i)
+		{
+			_putchar(' ');
+			j++;
+		}
+
+		j = 0;
+		while (j < i)
+		{
+			_putchar('#');
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
+	if (i == 1)
+		_putchar('\n');
+}
