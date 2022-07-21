2d5326068d75448217e0c7854ac613adc9e8b362
habideen <habideenibrahim@gmail.com>
2022-07-20 19:28:45 -0700

    Compare two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* wildcmp - compares two strings, and returns 1 if identical
+* wildcmp - compares two strings,  and returns 1 if identical
 * @s1: string to be checked
 * @s2: pattern to be used
 *
