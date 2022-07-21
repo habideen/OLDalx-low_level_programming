8924ad5ca3cb132ee212d7e6b9409026dfd5ae43
habideen <habideenibrahim@gmail.com>
2022-07-20 18:58:38 -0700

    y Power of x

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _pow_recursion - function that returns the
+* value of x raised to the power of y
+* @x: the base
+* @y: the exponent
+* Return: x**y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	/* base case */
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+
+	return (_pow_recursion(x, y - 1) * x);
+}
