cf6c789206a3eda5bc70a8583fb0cb6021eaffc4
habideen <habideenibrahim@gmail.com>
2022-07-26 21:23:31 -0700

    Creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* create_array - function to creates an array of chars,
+* and initializes it with a specific char.
+* @size: Size of the array
+* @c: Character to insert
+* Return: NULL if size is zero/fails,
+* pointer to array if everything is normal.
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *this_array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	this_array = malloc(sizeof(char) * size);
+
+	if (this_array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+	{
+		this_array[index] = c;
+	}
+
+	return (this_array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+* void prototypes()
+* char prototypes()
+* int prototypes()
+*/
+char *create_array(unsigned int size, char c);
+
+#endif /*MAIN_H*/
