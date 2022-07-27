1517e7b40b3df2a592e4b7ec2a998a69064c0e79
habideen <habideenibrahim@gmail.com>
2022-07-26 21:40:38 -0700

    Creates an array of chars, and in

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _strdup - this returns a pointer to a newly allocated
+* space in memory, which contains a copy of the
+* string given as a parameter.
+* @str: String to be copied
+* Return: NULL if error or pointer to allocated space
+*/
+
+char *_strdup(char *str)
+{
+	char *_copy;
+	int index, length;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		length++;
+    	}
+	_copy = malloc(sizeof(char) * (length + 1));
+
+	if (_copy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		_copy[index] = str[index];
+	}
+
+	_copy[length] = '\0';
+
+	return (_copy);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,6 @@
 * int prototypes()
 */
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif /*MAIN_H*/
